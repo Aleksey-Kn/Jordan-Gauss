b459d0be71846691f575a348ee440bf44150f676
Aleksey-Kn <aleksey.k2000@mail.ru>
2021-02-08 18:37:55 +0700

    Реализован парсинг исходного файла и анализ результирующей матрицы

--- src/Gaus.java
+++ src/Gaus.java
@@ -6,8 +6,20 @@ public class Gaus {
         Scanner fileScanner = new Scanner("file.txt");
         SimpleFraction[][] last = new SimpleFraction[fileScanner.nextInt()][fileScanner.nextInt()];
         SimpleFraction[][] now = new SimpleFraction[last.length][last[0].length];
+        String[] s;
+        for(int i = 0; i < last.length; i++){
+            for(int j = 0; j < last[0].length; j++){
+                s = fileScanner.next().split("/");
+                if(s.length == 1){
+                    last[i][j] = new SimpleFraction(Integer.parseInt(s[0]));
+                } else{
+                    last[i][j] = new SimpleFraction(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
+                }
+            }
+        }
         for(int k = 0; k < last.length; k++){
             if(!last[k][k].isNull()){
+                printMatrix(last);
                 for(int j = 0; k < last.length; k++){
                     if(j == k){
                         now[j][k] = new SimpleFraction(1);
@@ -23,7 +35,18 @@ public class Gaus {
                 last = now;
             }
         }
-
+        int[] count = new int[last.length];
+        boolean[][] contains = new boolean[last.length][last[0].length];
+        for(int i = 0; i < last.length; i++){
+            count[i] = (int)Arrays.stream(last[i]).limit(last[0].length - 1).filter(o -> !o.isNull()).count();
+            for (int j = 0; j < last[0].length - 1; j++){
+                contains[i][j] = !last[i][j].isNull();
+            }
+        }
+        if(Arrays.stream(count).anyMatch(o -> o == 0)){
+            System.out.println("Нет решений");
+            return;
+        }
     }
 
     private static void printMatrix(SimpleFraction[][] matrix){
